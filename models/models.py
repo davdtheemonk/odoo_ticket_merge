d27f41091786d3b3bcb07ae1cb78483e8fda3e7a
ProbablyX <83953506+davdtheemonk@users.noreply.github.com>
2023-08-17 16:21:12 +0300

    merging

--- models/models.py
+++ models/models.py
@@ -1,16 +1,45 @@
-from odoo import api, fields, models
+from odoo import models, fields, api
 
 class TicketMergeWizard(models.TransientModel):
     _name = 'ticket.merge.wizard'
-    _description = 'Wizard to merge tickets'
-
-    ticket_ids = fields.Many2many('helpdesk.ticket', string='Tickets to Merge')
+    _description = 'Ticket Merge Wizard'
+    
+    main_ticket_id = fields.Many2one('helpdesk.ticket', string='Main Ticket', required=True)
+    secondary_ticket_ids = fields.Many2many('helpdesk.ticket', string='Secondary Tickets')
 
     def merge_tickets(self):
-        self.ensure_one()
-        # You might want to implement a more sophisticated merging logic
-        main_ticket = self.ticket_ids[0]
-        for ticket in self.ticket_ids[1:]:
-            main_ticket.description += "\n\n" + ticket.description
-            ticket.unlink()
-        return {}
+        main_ticket = self.main_ticket_id
+        secondary_tickets = self.secondary_ticket_ids
+
+        # Merge information from secondary tickets into the main ticket
+        for secondary_ticket in secondary_tickets:
+            main_ticket.name += "," + secondary_ticket.name
+            secondary_ticket.message_post(body='This ticket has been merged into another ticket.')
+            secondary_ticket.unlink()
+
+        # Optional: Update other fields as needed (e.g., priority, status, etc.)
+
+        # Refresh the main ticket's display_name to reflect the changes
+        main_ticket.invalidate_cache()
+
+        return {'type': 'ir.actions.act_window_close'}
+    
+class HelpdeskTicket(models.Model):
+    _inherit = 'helpdesk.ticket'
+    _name = "ticket.helpdesk.in"
+
+    merged_tickets = fields.Text(string='Merged Tickets', compute='_compute_merged_tickets')
+    sla_ids = fields.Many2many(
+    'helpdesk.sla',
+    'ticket_helpdesk_in_sla_rel',
+    'ticket_id',
+    'sla_id',
+)
+    @api.depends('name', 'message_ids')
+    def _compute_merged_tickets(self):
+        for ticket in self:
+            merged_ticket_names = []
+            for message in ticket.message_ids.filtered(lambda msg: msg.subtype_id.internal):
+                if 'merged into another ticket' in message.body:
+                    merged_ticket_names.append(message.model)
+            ticket.merged_tickets = '\n'.join(merged_ticket_names)
